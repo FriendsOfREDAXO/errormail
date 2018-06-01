239d130dc9e4bdff6462ab419de55c6a1c872385
Thomas Skerbis <thomas.skerbis@mac.com>
2018-06-01 21:34:09 +0200

    Sends only Exceptions, Errors and custom logevents

--- boot.php
+++ boot.php
@@ -5,7 +5,6 @@
  *
  * @author Friends of REDAXO
  *
- * @package redaxo\phpmailer
  */
 
 if (!rex::isBackend()) {
@@ -13,13 +12,15 @@ if (!rex::isBackend()) {
     {
         $logFile    = rex_path::coreData('system.log');
         $sendTime   = $this->getConfig('last_log_file_send_time', 0);
+        $fileTime   = filemtime($logFile);
         $timediff   = '';
         $fatalerror = false;
+        $logevent = false;
         $timediff   = time() - $sendTime;
         if ($timediff > 3 && filesize($logFile) > 0 && $file = new rex_log_file($logFile)) {
             //Start - generate mailbody
             $mailBody = '';
-            $mailBody .= '<style> .eventbg {background: #F6C4AF; } td, th {padding: 5px;} table {width: 100%; border: 1px solid #ccc; } th {background: #b00; color: #fff;} td { border: 0; border-bottom: 1px solid #b00;} </style> ';
+            $mailBody .= '<style> .errorbg {background: #F6C4AF; } .eventbg {background: #E1E1E1; } td, th {padding: 5px;} table {width: 100%; border: 1px solid #ccc; } th {background: #b00; color: #fff;} td { border: 0; border-bottom: 1px solid #b00;} </style> ';
             $mailBody .= '<table>';
             $mailBody .= '    <thead>';
             $mailBody .= '        <tr>';
@@ -35,14 +36,25 @@ if (!rex::isBackend()) {
                 /* @var rex_log_entry $entry */
                 $data  = $entry->getData();
                 $style = '';
-                if ($data[0] == 'Fatal error' || $data[0] == 'logevent' || $data[0] == 'Exeption' ) {
+                $logtypes = array('error', 'exception');
+
+
+                foreach($logtypes as $type) {
+                    if(stripos($data[0], $type) !== false) {
+                        $logevent = true;
+                        $style      = ' class="errorbg"';
+                        break;
+                    }
+                }
+
+                if ($data[0]=='logevent') {
                     $style      = ' class="eventbg"';
                     $logevent = true;
                 }
                 $mailBody .= '        <tr' . $style . '>';
                 $mailBody .= '            <td>' . $entry->getTimestamp('%d.%m.%Y %H:%M:%S') . '</td>';
                 $mailBody .= '            <td>' . $data[0] . '</td>';
-                $mailBody .= '            <td>' . substr($data[1], 0, 128) . '</td>';
+                $mailBody .= '            <td>' . substr(rex_escape($data[1]), 0, 128) . '</td>';
                 $mailBody .= '            <td>' . (isset($data[2]) ? $data[2] : '') . '</td>';
                 $mailBody .= '            <td>' . (isset($data[3]) ? $data[3] : '') . '</td>';
                 $mailBody .= '        </tr>';

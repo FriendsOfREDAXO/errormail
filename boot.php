eb0751a3b2c22f23f2b173620a44cb725905c9bc
Thomas Skerbis <thomas.skerbis@mac.com>
2018-06-01 19:52:24 +0200

    only fatal errors + highlighting
    /Users/thomas/Desktop/Bildschirmfoto 2018-06-01 um 19.51.14.png

--- boot.php
+++ boot.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * PHPMailer Addon.
+ * errormail Addon.
  *
- * @author markus[dot]staab[at]redaxo[dot]de Markus Staab
+ * @author Friends of REDAXO
  *
  * @package redaxo\phpmailer
  */
@@ -11,14 +11,15 @@
 if (!rex::isBackend()) {
     rex_extension::register('RESPONSE_SHUTDOWN', function(rex_extension_point $ep)
     {
-        $logFile  = rex_path::coreData('system.log');
-        $sendTime = $this->getConfig('last_log_file_send_time', 0);
-        $timediff = '';
-        $timediff = time() - $sendTime;
-        if ($timediff > 900 && filesize($logFile) > 0 && $file = new rex_log_file($logFile)) {
+        $logFile    = rex_path::coreData('system.log');
+        $sendTime   = $this->getConfig('last_log_file_send_time', 0);
+        $timediff   = '';
+        $fatalerror = false;
+        $timediff   = time() - $sendTime;
+        if ($timediff > 3 && filesize($logFile) > 0 && $file = new rex_log_file($logFile)) {
             //Start - generate mailbody
             $mailBody = '';
-            $mailBody .= '<style>  td, th {padding: 5px;} table {width: 100%; border: 1px solid #ccc; } th {background: #b00; color: #fff;} td { border: 0; border-bottom: 1px solid #b00;} </style> ';
+            $mailBody .= '<style> .eventbg {background: #F6C4AF; } td, th {padding: 5px;} table {width: 100%; border: 1px solid #ccc; } th {background: #b00; color: #fff;} td { border: 0; border-bottom: 1px solid #b00;} </style> ';
             $mailBody .= '<table>';
             $mailBody .= '    <thead>';
             $mailBody .= '        <tr>';
@@ -32,30 +33,42 @@ if (!rex::isBackend()) {
             $mailBody .= '    <tbody>';
             foreach (new LimitIterator($file, 0, 30) as $entry) {
                 /* @var rex_log_entry $entry */
-                $data = $entry->getData();
-                $mailBody .= '        <tr>';
+                $data  = $entry->getData();
+                $style = '';
+                if ($data[0] == 'Fatal error') {
+                    $style      = ' class="eventbg"';
+                    $logevent = true;
+                }
+                if ($data[0] == 'logevent') {
+                    $style      = ' class="eventbg"';
+                    $logevent = true;
+                }
+                $mailBody .= '        <tr' . $style . '>';
                 $mailBody .= '            <td>' . $entry->getTimestamp('%d.%m.%Y %H:%M:%S') . '</td>';
                 $mailBody .= '            <td>' . $data[0] . '</td>';
-                $mailBody .= '            <td>' . substr($data[1],0,128) . '</td>';
+                $mailBody .= '            <td>' . substr($data[1], 0, 128) . '</td>';
                 $mailBody .= '            <td>' . (isset($data[2]) ? $data[2] : '') . '</td>';
                 $mailBody .= '            <td>' . (isset($data[3]) ? $data[3] : '') . '</td>';
                 $mailBody .= '        </tr>';
             }
-            $mailBody .= '    </tbody>';
-            $mailBody .= '</table>';
-            //End - generate mailbody
-            //Start  send mail
-            $mail          = new rex_mailer();
-            $mail->Subject = rex::getServerName() . ' | system.log';
-            $mail->Body    = $mailBody;
-            $mail->AltBody = strip_tags($mailBody);
-            $mail->setFrom(rex::getErrorEmail(), 'REDAXO Errormail');
-            $mail->addAddress(rex::getErrorEmail());
-            $this->setConfig('last_log_file_send_time', $fileTime);
-            if ($mail->Send()) {
-               // mail has been sent
+            // check if fatal error occured
+            if ($fatalerror == true or $logevent == true) {
+                $mailBody .= '    </tbody>';
+                $mailBody .= '</table>';
+                //End - generate mailbody
+                //Start  send mail
+                $mail          = new rex_mailer();
+                $mail->Subject = rex::getServerName() . ' | system.log';
+                $mail->Body    = $mailBody;
+                $mail->AltBody = strip_tags($mailBody);
+                $mail->setFrom(rex::getErrorEmail(), 'REDAXO Errormail');
+                $mail->addAddress(rex::getErrorEmail());
+                $this->setConfig('last_log_file_send_time', $fileTime);
+                if ($mail->Send()) {
+                    // mail has been sent
+                }
             }
-             // close logger, to free remaining file-handles to syslog
+            // close logger, to free remaining file-handles to syslog
             rex_logger::close();
             //End  send mail
         }

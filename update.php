b8c8e534090bb9e228f2f056b242ecfc6c787cf1
Thomas Skerbis <thomas.skerbis@mac.com>
2018-06-02 03:16:22 +0200

    update from 1.x

---
+++ update.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * errormail Addon.
+ *
+ * @author Friends of REDAXO
+ *
+ */
+if (!$this->hasConfig('errormail_intval')) {
+        $this->setConfig('errormail_intval', 900);
+}
+
+
+
